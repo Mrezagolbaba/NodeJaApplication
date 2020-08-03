250e7a4378e4dca56bf7739a59bbdb05db2fad7d
mohamad <m.reza.golbaba@gmail.com>
2020-08-03 19:54:24 +0430

    update products models

--- api/models/product.js
+++ api/models/product.js
@@ -19,20 +19,31 @@ const getProductsFromFile = cb =>{
 };
 
 module.exports = class Product {
-	constructor(t){
-		this.title = t;	
+	constructor(title, imageUrl, description, price){
+		this.title = title;	
+		this.imageUrl = imageUrl;	
+		this.description = description;	
+		this.price = price;	
 	}
 
 	save (){
+		this.id= Math.random().toString()
 		getProductsFromFile(products =>{
-			products.push(this)
+			products.push(this);
 			fs.writeFile(p,JSON.stringify(products),err=>{
-				console.log(err)
+				console.log(err);
 			});
 		})
 	}
 
 	static fetchAll (cb) {
-		getProductsFromFile(cb)
+		getProductsFromFile(cb);
+	}
+
+	static findById(id, cb){
+		getProductsFromFile(products=>{
+			const product =products.find(p =>p.id ===id);
+			cb(product);
+		})
 	}
 }
