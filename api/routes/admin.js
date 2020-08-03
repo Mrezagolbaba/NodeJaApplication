16bc2007ae7f053c65539d914b7ab69c49a34f12
mohamad <m.reza.golbaba@gmail.com>
2020-08-03 19:54:06 +0430

    update admin routes

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const express = require('express');
-
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 const router =express.Router(); 
 
+// /admin/add-products => GET
+router.get('/add-product',adminController.getAddProduct);
 
-router.get('/add-product',productsController.getAddProduct);
+// /admin/products => GET
+router.get('/products',adminController.getProducts);
 
-router.post('/add-product',productsController.postAddProduct);
+// /admin/add-products => POST
+router.post('/add-product',adminController.postAddProduct);
 
 
 module.exports = router;
