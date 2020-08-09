197d89f06a27ea783711bd9a32bb348357fc684a
mohamad <m.reza.golbaba@gmail.com>
2020-08-09 21:53:57 +0430

    update shop controller

--- api/controllers/shop.js
+++ api/controllers/shop.js
@@ -11,11 +11,14 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId
-  Product.findById(prodId,product=>{
-    res.render()
-  })
- 
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
@@ -28,17 +31,23 @@ exports.getIndex = (req, res, next) => {
   });
 };
 
-exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+exports.postCart = (req, res, next) => {
+ const prodId = req.body.productId;
+ console.log("prodId",prodId)
+ res.redirect('/cart')
 };
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
   });
 };
 
