6ab6b13d048e058d6d4e6180007fd31495892286
mohamad <m.reza.golbaba@gmail.com>
2020-08-11 00:21:18 +0430

    update route

--- api/routes/shop.js
+++ api/routes/shop.js
@@ -1,9 +1,8 @@
 const path = require('path')
 const express = require('express');
 
-
-
 const shopController = require ('../controllers/shop')
+
 const router =express.Router();
 
 
@@ -12,9 +11,11 @@ router.get('/',shopController.getIndex);
 
 router.get('/products',shopController.getProducts)
 
-router.get('/products/:productId',shopController.getProducts)
+router.get('/products/:productId',shopController.getProduct)
 
 router.get('/cart',shopController.getCart)
+
+router.post('/cart',shopController.postCart)
 
 router.get('/orders',shopController.getOrders)
 
